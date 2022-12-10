2d81931d88a8199f91bfbe871bb3574de5541d5c
TinaZavvar <tnazavvar@gmail.com>
2022-12-10 10:51:07 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://Admin:V1EIQ5P0GFrEZ7uu@cluster0.jd5jho2.mongodb.net/PhotoSharing?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+  };
+  
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,15 @@
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
+
+module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+  },
+  Query: {
+    ...postsResolvers.Query
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+    ...postsResolvers.Mutation,
+  }
+};
