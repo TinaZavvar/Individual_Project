d6221ca117f512cc4d5a04f6b445327263e8be00
TinaZavvar <tnazavvar@gmail.com>
2022-12-10 14:42:01 -0500

    Initial commit

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+// const mongoDbPassword = process.env.MONGODB_PASSWORD;
+
+
 module.exports = {
     MONGODB:
       'mongodb+srv://Admin:V1EIQ5P0GFrEZ7uu@cluster0.jd5jho2.mongodb.net/PhotoSharing?retryWrites=true&w=majority',
