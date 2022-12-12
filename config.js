b729ce8846702037764680c6b32e7e922b924d80
Tina Zavvar <104872938+TinaZavvar@users.noreply.github.com>
2022-12-12 17:20:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     MONGODB:
-      'mongodb+srv://Admin:V1EIQ5P0GFrEZ7uu@cluster0.jd5jho2.mongodb.net/PhotoSharing?retryWrites=true&w=majority',
+      'mongodb+srv://Admin:{Pass}@cluster0.jd5jho2.mongodb.net/PhotoSharing?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
   };
   
